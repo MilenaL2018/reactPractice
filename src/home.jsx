175212fa834fac878c0cd8900c73b59cca4c2254
milel <milelucero_09@hotmail.com>
2020-03-23 13:49:32 -0300

    sidebar structure defined, need to change color to #520029

---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SideNav, {NavIcon, NavItem, NavText} from "@trendmicro/react-sidenav";
+
+
+class Home extends Component {
+    render() {
+        return (
+                <h1>Home</h1>
+        )
+    }
+}
+
+
+export default Home;
